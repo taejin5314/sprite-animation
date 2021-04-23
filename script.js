ab2e0d8b46acaa538549b4b340f9c6340cc475b8
taejin <taejin5314@gmail.com>
2021-04-23 18:01:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(100, 50, 100, 100);
-  ctx.drawImage(playerImage, 1 * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   requestAnimationFrame(animate);
 }
 
