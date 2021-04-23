0cc80533cdb6497adcd5d876053f3e9766fd7116
taejin <taejin5314@gmail.com>
2021-04-23 18:09:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
+const staggerFrame = 2;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  // ctx.fillRect(100, 50, 100, 100);
-  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-  if (gameFrame % 2 === 0) {
+  let position = Math.floor(gameFrame / staggerFrame) % 6;
+  frameX = spriteWidth * position;
+  ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame % staggerFrame === 0) {
     if (frameX < 6) frameX++;
     else frameX = 0;
   }
