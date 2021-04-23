2c53b6222bf2f2e637058eb2642ad1fe72c48dbc
taejin <taejin5314@gmail.com>
2021-04-23 18:05:46 -0400

    set the game frame variable

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(100, 50, 100, 100);
   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame % 2 === 0) {
+    if (frameX < 6) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
