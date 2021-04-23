11ab2ebc1d71ddd605f1bd422cc909c2beea11ea
taejin <taejin5314@gmail.com>
2021-04-23 18:00:20 -0400

    crop sprite image

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(100, 50, 100, 100);
-  ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(playerImage, 1 * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   requestAnimationFrame(animate);
 }
 
